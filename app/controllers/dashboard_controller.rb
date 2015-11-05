4770c15bb9e949666eacf40d5bd861d036b102ff
Doug Bryant <douglas.e.bryant@gmail.com>
2015-11-05 12:17:06 -0500

    Add AJAX for student to submit scores and revise reset method

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,10 +3,17 @@ class DashboardController < ApplicationController
 
   def teacher_dashboard
     @score_average = Student.score_avg
-    @reset = Student.reset_score
   end
 
 
   def student_dashboard
+  end
+
+  # For this to work...
+  #   * set up a post route to get here
+  #   * change the link in the view to come here instead
+  def reset
+    Student.reset_score
+    redirect_to teacher_dashboard_path
   end
 end
