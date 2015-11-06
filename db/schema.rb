7bb38da0dc6a1e5616e69f2181e57efc0388baf3
Ryan Graham <ryangrahamwpb@gmail.com>
2015-11-06 16:28:01 -0500

    Teacher can create, edit and destroy topics.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151106175003) do
+ActiveRecord::Schema.define(version: 20151106205709) do
 
   create_table "scores", force: :cascade do |t|
     t.integer  "score"
