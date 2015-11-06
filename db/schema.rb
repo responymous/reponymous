bba63a8f6b5b0951e9c571a6775a5b294d607de9
Doug Bryant <douglas.e.bryant@gmail.com>
2015-11-06 16:39:56 -0500

    Reattempt remove score field from Students table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151106205709) do
+ActiveRecord::Schema.define(version: 20151106211221) do
 
   create_table "scores", force: :cascade do |t|
     t.integer  "score"
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 20151106205709) do
     t.string   "email"
     t.string   "password_digest"
     t.integer  "teacher_id"
-    t.integer  "score"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
