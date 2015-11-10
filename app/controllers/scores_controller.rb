3d4b879c8ccd80fc5ff35f967f321330a26cdd81
Doug Bryant <douglas.e.bryant@gmail.com>
2015-11-10 11:25:26 -0500

    Add patch route for student score update

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -2,9 +2,10 @@ class ScoresController < ApplicationController
   def change
     s = Score.where(student_id: params[:student_id], topic_id: params[:topic_id]).first
     if s != nil
-      s.update(score: params[:score])
+      s.update!(score: params[:score])
     else
-      Score.create!(score: params[:score], student_id: params [:student_id], topic_id: params[:topic_id])
+      Score.create!(score: params[:score], student_id: params[:student_id], topic_id: params[:topic_id])
     end
+    render nothing: true
   end
 end
